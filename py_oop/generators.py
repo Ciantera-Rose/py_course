297ab2c78e0fb6fc0b4a115d166c9b7447ec1b87
Ciantera-Rose <ciantera.rose@gmail.com>
2020-11-24 18:55:58 -0500

    generators

---
+++ py_oop/generators.py
@@ -0,0 +1,62 @@
+#Overview of Iterators vs Generators in Python
+#A generator allows you to yield an iteration to a particiluar method until you call it again
+
+
+class InfiniteLineup:
+    def __init__(self, players): #Starts w/ an init method
+        self.players = players #Lets us bring in players list
+
+    def lineup(self):
+        lineup_max = len(self.players) #Brings in total count of players that are there
+        idx = 0 #Sets index to 0      
+
+#Creates inifinte loop
+
+        while True:
+            if idx < lineup_max:
+                yield self.players[idx] #Generators use yield keyword. Yields to generator. Python creates a generator object
+            else:
+                idx = 0 #Resets index
+                yield self.players[idx]
+
+            idx += 1 #While inside loop and increment index by 1
+
+    def __repr__(self):
+        return f'InfiniteLineup({self.players})'
+#Repr functions takes in self and returns entire object
+
+    def __str__(self):
+        return f"InfiniteLineup with the players: {', '.join(self.players)}"
+#Dunder string method: We pass in list of players in the argument
+
+astros = [
+  'Springer',
+  'Bregman',
+  'Altuve',
+  'Correa',
+  'Reddick',
+  'Gonzalez',
+  'McCann',
+  'Davis',
+  'Tucker'
+]
+
+full_lineup = InfiniteLineup(astros) #This instantiates our list of platers
+astros_lineup = full_lineup.lineup() #Needs to call lineup method
+
+print(next(astros_lineup))
+print(next(astros_lineup))
+print(next(astros_lineup))
+print(next(astros_lineup))
+print(next(astros_lineup))
+print(next(astros_lineup))
+print(next(astros_lineup))
+print(next(astros_lineup))
+print(next(astros_lineup))
+print(next(astros_lineup))
+print(next(astros_lineup))
+print(next(astros_lineup))
+
+print(repr(full_lineup))
+
+print(str(full_lineup))
