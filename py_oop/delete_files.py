45c035d08b5a467a616ebdb97a2318cedacfc9f3
Ciantera-Rose <ciantera.rose@gmail.com>
2020-11-25 15:56:18 -0500

    delete files

---
+++ py_oop/delete_files.py
@@ -0,0 +1,7 @@
+#How to Delete Files in Python
+
+import os #operating system module. Allows to interact with whatever operating system we are working withh
+
+os.remove("file-section/file_to_be_deleted.txt") #Call file script from root of program
+
+print("File Removed!")
